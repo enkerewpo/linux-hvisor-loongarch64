940650a0247b9b46231cf7d2068d68592bf7bfc2
wheatfox <enkerewpo@hotmail.com>
2024-07-16 11:00:39 +0800

    update

--- rootfs/busybox/include/autoconf.h
+++ rootfs/busybox/include/autoconf.h
@@ -2,7 +2,7 @@
  * Automatically generated C config: don't edit
  * Busybox version: 1.36.1
  */
-#define AUTOCONF_TIMESTAMP "2024-07-09 16:07:39 CST"
+#define AUTOCONF_TIMESTAMP "2024-02-29 17:15:02 CST"
 
 #define CONFIG_HAVE_DOT_CONFIG 1
 #define ENABLE_HAVE_DOT_CONFIG 1
