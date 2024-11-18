43aa1c4039ad24a3ff69ff26117dfac0455ac59b
wheatfox <enkerewpo@hotmail.com>
2024-11-18 21:13:21 +0800

    update

--- rootfs/my_init/init.c
+++ rootfs/my_init/init.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int main() {
-    int wait_time = 2;
-    for (int i = 0; i < wait_time; i++) {
-        printf("Waiting for %d more seconds...\n", wait_time - i);
-        sleep(1);
+int main()
+{
+    // print something into /dev/hvc0 for testing virtio console
+    const char *target_device = "/dev/hvc0";
+    FILE *f = fopen(target_device, "w");
+    if (f == NULL)
+    {
+        printf("Failed to open %s\n", target_device);
+        return 1;
     }
-    int pid = getpid();
-    printf("init process pid: %d\n", pid);
-    system("uname -a");
-    printf("Hello, world from my userspace init! this is for testing input/output to virtio console\n");
-    char buf[1024];
-    while(1) {
-        printf("> ");
-        fgets(buf, 1024, stdin);
-        printf("You entered: %s", buf);
-        if (buf[0] == 'q') {
-            break;
-        }
+    printf("Writing to %s\n", target_device);
+    int count = 10;
+    while (count--)
+    {
+        fprintf(f, "Hello, world!\n");
     }
-    printf("Goodbye! (but we're not actually going anywhere)\n");
-    while(1) {}
+    fflush(f);
+    fclose(f);
+
+    // can't reach here
+    while (1)
+        ;
     return 0;
 }
