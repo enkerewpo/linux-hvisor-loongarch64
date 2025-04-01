975625f24194480eb4c18efa3bda6e119fc1a776
wheatfox <enkerewpo@hotmail.com>
2025-04-01 18:57:26 +0800

    add yt6801 patching script

---
+++ patch.sh
@@ -0,0 +1,5 @@
+# the yt6801 patch, however we cannot successfully apply it by now (2025.4.1) - wheatfox
+b4 am -o patch 1482683f626c0743e3ec53161dd291de3a6726f6.camel@mailbox.org
+cd linux-6.13.7
+git am ../patch/v3_20250228_frank_sae_net_yt6801_add_motorcomm_yt6801_pcie_driver.mbx
+# git am --abort # if you want to abort the patch
