3e7144e6622d3530668c5d46beacf748d4bd6c3e
wheatfox <enkerewpo@hotmail.com>
2025-04-03 15:09:16 +0800

    update .gitignore and linux-git submodule reference; enhance patch script for better organization

--- patch.sh
+++ patch.sh
@@ -1,3 +1,5 @@
+rm -rf patch
+mkdir patch
 # the yt6801 patch, however we cannot successfully apply it by now (2025.4.1) - wheatfox
 b4 am -o patch 1482683f626c0743e3ec53161dd291de3a6726f6.camel@mailbox.org
 # cd linux-6.13.7
