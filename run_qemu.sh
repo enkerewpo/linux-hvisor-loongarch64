5bf9f5f574a9409e8ece44a50712ddcc62b0ce45
wheatfox <enkerewpo@hotmail.com>
2025-06-24 19:37:37 +0800

    Refactor build scripts and update device tree configurations for Loongson 3A5000 hvisor
    - Removed obsolete Linux version from .ignore file.
    - Enhanced build script to provide verbose output during file copying.
    - Updated device tree files to correct UART aliasing for proper serial communication.
    - Modified kernel configuration files to include Rust support and optimize network filtering options.

--- run_qemu.sh
+++ run_qemu.sh
@@ -2,13 +2,9 @@
 
 # https://www.qemu.org/docs/master/system/loongarch/virt.html
 
-CHOSEN=$(cat chosen)
-KERNEL_PATH=linux-${CHOSEN}/vmlinux
+CHOSEN_ROOT=$(cat chosen_root)
+KERNEL_PATH=linux-${CHOSEN_ROOT}/vmlinux
 
 qemu-system-loongarch64 -machine virt -m 16G -cpu la464 \
     -smp 1 -kernel $KERNEL_PATH \
-    -device igb,netdev=net0,bus=pcie.0,addr=0x6 \
-    -netdev user,id=net0 \
-    -device nvme,drive=nvme0,bus=pcie.0,addr=0x5,serial=1234567890 \
-    -drive file=nvme_ext4.img,if=none,id=nvme0,format=raw \
     -serial mon:stdio --nographic
